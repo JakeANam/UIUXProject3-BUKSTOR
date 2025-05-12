d1eff1482d9da63248de5f2106a5e3bf5a4ab3de
Eo-Jin Nam (Jake) <nams93627@naver.com>
2025-05-12 17:49:05 +0900

    Apply .gitignore

--- scripts/commonScripts.js
+++ scripts/commonScripts.js
@@ -1,14 +1,35 @@
 // 공통 script
+
+fixHeader();
+function fixHeader(){
+    if (window.scrollY >= 25) {
+        let headerBottom = this.document.getElementsByClassName("headerBottom")[0];
+        headerBottom.style.position = "fixed";
+        headerBottom.style.top = '0';
+        headerBottom.style.left = (-1 * this.window.scrollX) + "px"
+    }
+}
+
+
 // 스크롤시 헤더 고정
 window.addEventListener("scroll", function(){
     let headerBottom = this.document.getElementsByClassName("headerBottom")[0];
+    
     if(this.window.scrollY >= 25) {
-        headerBottom.style.position = "fixed";
-        headerBottom.style.top = '0';
+        fixHeader();
+        // headerBottom.style.position = "fixed";
+        // headerBottom.style.top = '0';
+        // headerBottom.style.left = (-1 * this.window.scrollX) + "px"
+        //headerBottom.style.left = "50%";
+        //headerBottom.style.transform = "translateX(-50%)"
     } else {
         headerBottom.style.position = "relative";
+        headerBottom.style.left = 0;
+        // headerBottom.style.transform = "translateX(0)"
     }
+
 });
+
 
 // 책 이미지 높이 설정(너비:높이 = API 특성상 2:3)
 function mainBookThumbnailResizing(){
@@ -20,5 +41,7 @@ function mainBookThumbnailResizing(){
     }
 }
 
-window.addEventListener('resize', mainBookThumbnailResizing);
+window.addEventListener('resize', function(){
+    mainBookThumbnailResizing();
+});
 
